238c675e0f96859cffeb5f24523fe4a58eec0089
samillette <sam.al.millette@gmail.com>
2020-11-12 16:52:20 -0500

    added in pair randomization file, could later be added into main file, or modified to become a standalone class

---
+++ randompairs.cpp
@@ -0,0 +1,31 @@
+#include <utility>
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <string>
+
+std::vector< std::pair<int, int> > coordinatePairs;
+
+void generatePairs( int seed, int cardinality ){
+    srand(seed);
+    for(int i = 0; i < cardinality; i++){
+        std::pair<int, int> coordinate;
+        // uncomment this if we will ever need x and y later
+        //int x = rand() % 100;
+        //int y = rand() % 100;
+        coordinate.first = rand() % 100;
+        coordinate.second = rand() % 100;
+        coordinatePairs.push_back(coordinate);
+    }
+
+}
+
+int main(int argc, char**argv) {
+    int seed = std::stoi(argv[1]);
+    int numPairs = std::stoi(argv[2]);
+    generatePairs( seed, numPairs );
+    for( int i = 0; i < numPairs; i++){
+        std::cout << coordinatePairs[i].first << ", " << coordinatePairs[i].second << std::endl;
+    }
+}
+
