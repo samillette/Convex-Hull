e0d48d62cc8cea2325ef37292af911f211cdde02
samillette <sam.al.millette@gmail.com>
2020-12-06 17:44:52 -0500

    changed main file to allow for manual file entering - Connor add something into the readme about how to structure a csv file for these algorithms - and to allow for random file creation. Also added in the kobe dataset

--- Source Code/main.cpp
+++ Source Code/main.cpp
@@ -5,26 +5,35 @@
 #include <iostream>
 
 int main(){
-    std::cout << "Number of items in random set: ";
-    int items;
-    std::cin >> items;
-    std::cout << "Please enter the seed: ";
-    int seed;
-    std::cin >> seed;
 
-    generatePairs( seed, items );
+    std::cout << "Enter 'random' for a random set of numbers or enter the filepath of the csv file you wish to analyze: ";
+    std::string decisionOrFile;
+    std::cin >> decisionOrFile;
+
+    if( decisionOrFile == "random" ) {
+        std::cout << "Number of items in random set: ";
+        int items;
+        std::cin >> items;
+        std::cout << "Please enter the seed: ";
+        int seed;
+        std::cin >> seed;
+
+        generatePairs( seed, items );
+        decisionOrFile = "randomPairs.csv";
+    }
 
     std::cout << "For graham scan algorithm enter 1. For quickhull algorithm enter 2: ";
-    int decision;
-    std::cin >> decision;
-    if( decision == 1 ) {
-        //graham( "randomPairs.csv" );
+    int algorithm;
+    std::cin >> algorithm;
+    if( algorithm == 1 ) {
+        //graham( decisionOrFile );
         return 1;
     }
     else{
-        quickHull( "randomPairs.csv" );
+        quickHull( decisionOrFile );
     }
 
-
+    std::cout << "Depending on if you called graham scan or quickhull the output file will be named either XXX or quickhullPairs.csv" << std::endl;
+    std::cout << "To visualize the data, run 'python3 visualization.py'. The file will ask for the name of the file you wish to analyze, see previous line." << std::endl;
     return 1;
 }
